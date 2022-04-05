30f9e50dfabca96b9a00b7959c5863e837e5c102
brytebee <brytebee@gmail.com>
2022-04-05 17:21:54 +0100

    :white_check_mark: Modify class
    Added default value params

--- animal.rb
+++ animal.rb
@@ -1,13 +1,13 @@
 class Animal
-  def initialize(name, number_of_legs)
+  def initialize(number_of_legs, name = 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
   end
 end
 
-animal_1 = Animal.new
-animal_2 = Animal.new
+animal_1 = Animal.new("Rex", 8)
 
-animal_1
-animal_2
+p animal_1.id
+p animal_1.name
+p animal_1.number_of_legs
