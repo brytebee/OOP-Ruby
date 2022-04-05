f4dc8aa8d18809daedef23184c5dadb67a669791
brytebee <brytebee@gmail.com>
2022-04-05 18:48:58 +0100

    :white_check_mark: Add Animal#remove_leg
    Added Animal#remove_leg

--- animal.rb
+++ animal.rb
@@ -1,3 +1,5 @@
+require "./remover.rb"
+
 class Animal
   def initialize(type, number_of_legs, name = 'Unknown')
     @id = Random.rand(1..1000)
@@ -28,5 +30,10 @@ class Animal
 
   def name=(value)
     @name = value
+  end
+
+  def remove_leg
+    remover = Remover.new()
+    @number_of_legs = remover.decrease(@number_of_legs)
   end
 end
