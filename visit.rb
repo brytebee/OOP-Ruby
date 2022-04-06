944c55de2f67a1065b276ea35853cacfe8f67212
brytebee <brytebee@gmail.com>
2022-04-06 12:06:11 +0100

    :white_check_mark: Populate Visit
    Modifies class Visit

--- visit.rb
+++ visit.rb
@@ -1,2 +1,14 @@
-class Visit  
+class Visit
+  attr_reader :animal, :vet
+  attr_accessor :date
+
+  def initialize(date, animal, vet)
+    @date = date
+
+    @animal = animal
+    animal.visits << self
+
+    @vet = vet
+    vet.visits << self
+  end
 end
